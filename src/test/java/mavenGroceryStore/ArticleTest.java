cf890ecf704db7a4b8b5f429468e8cdc71b4f859
ShiftyGOSU <tom_karlsson94@hotmail.com>
2021-02-01 15:45:06 +0100

    add method to sum articles in cart and tests for methods

--- src/test/java/mavenGroceryStore/ArticleTest.java
+++ src/test/java/mavenGroceryStore/ArticleTest.java
@@ -6,10 +6,37 @@ import org.junit.Test;
 
 public class ArticleTest {
 	
+	private Cart myCart;
+	private Article art1;
+	private Article art2;
+	
+	// To simplify testing
+	public ArticleTest() {
+		myCart = new Cart();
+		art1 = new Article("Apple", 10);
+		art2 = new Article("Banana", 10);
+	}
+	
 	@Test
-	public void testName() {
-		Article apple = new Article("Apple", 8);
-		assertEquals("Apple", apple.getName());
+	public void testRightName() {
+		assertEquals("Apple", art1.getName());
+	}
+	
+	@Test
+	public void testRightPrice() {
+		assertEquals(10, art1.getPrice());
 	}
 
+    @Test
+    public void testRightSumArticlesInCart() {
+    	myCart.addItemToCart(art1);
+    	myCart.addItemToCart(art2);
+    	
+        assertEquals(20, Article.sumArticlesInCart(myCart.getCart()));
+    }
+
+    @Test
+    public void testWrongSumArticlesInCart() {
+        assertFalse(Article.sumArticlesInCart(myCart.getCart()) == 10);
+    }
 }
