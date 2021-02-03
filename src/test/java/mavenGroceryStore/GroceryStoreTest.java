e2248db68ffb38d6e17e689edae532061394c07b
Niklas Johansson <72798767+Nixinhas@users.noreply.github.com>
2021-02-03 15:30:30 +0100

    Update GroceryStoreTest.java
    Update GroceryStoreTest.java so that it's compatible with JUnit4

--- src/test/java/mavenGroceryStore/GroceryStoreTest.java
+++ src/test/java/mavenGroceryStore/GroceryStoreTest.java
@@ -5,30 +5,31 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class GroceryStoreTest {
+	private GroceryStore gs;
+
+	public GroceryStoreTest() {
+		gs = new GroceryStore("Ica Kvantum");
+	}
 
 	@Test
 	public void testName() {
-		GroceryStore gs = new GroceryStore("Ica Kvantum");
 		assertEquals("Ica Kvantum", gs.getName());
 	}
-	
+
 	@Test
 	public void testAddArticle() {
-		GroceryStore gs = new GroceryStore("Ica Kvantum");
 		gs.addArticle("Banana", 5);
 		assertEquals("Banana", gs.getArticles().get(0).getName());
 	}
-	
-	@Test
-	public void testAddArticleWithNegativePrice() { 
-		GroceryStore gs = new GroceryStore("Ica Kvantum");
-		assertThrows(IllegalArgumentException.class, () -> gs.addArticle("Banana", -10));
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testAddArticleWithNegativePrice() {
+		gs.addArticle("Banana", -10);
 	}
-	
-	@Test
+
+	@Test(expected = IllegalArgumentException.class)
 	public void testAddArticleThatAlreadyExist() {
-		GroceryStore gs = new GroceryStore("Ica Kvantum");
 		gs.addArticle("Banana", 5);
-		assertThrows(IllegalArgumentException.class, () -> gs.addArticle("BaNana ", 5));
+		gs.addArticle("Banana", 5);
 	}
 }
